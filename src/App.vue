3690b990959ba775f01ab114fe6dc9825878b0e4
mhendzel123 <mhendzel@wp.pl>
2020-04-25 10:05:59 +0200

    dlugosc_maila

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 
+
+
 <template>
   <div>
-	<h1>twoj email to {{ email }}</h1>
-	<h3>email  {{ email }}</h3>
-			<input type="email"
+	<h1>Twoj email to: {{ email }}</h1>
+	
+	<div v-if="email.length < 10">Ale masz krotki adres!</div>
+	<div v-else-if="email.length < 15">Twoj adres e-mail jest w sam raz.</div>
+	<div v-else>Twoj adres e-mail jest stanowczo za dlugi.</div>
+	
+		<input type="email"
 			v-model="email">
   </div>
 </template>
@@ -12,9 +18,9 @@
 
 <script>
 export default {
- data() {
- 	return {
- 		email: ''
+	data() {
+ 		return {
+ 			email: ''
  		};
  	}
 }
