90480160d3339395d551ba38d3e0289dcc32a325
mhendzel123 <mhendzel@wp.pl>
2020-04-25 11:01:28 +0200

    logout

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,27 @@
 
 
-
 <template>
   <div>
-	<h1>Twoj email to: {{ email }}</h1>
+  
+	<h1>Witaj w systemie zapisow</h1>
 	
-	<div v-if="email.length < 10">Ale masz krotki adres!</div>
-	<div v-else-if="email.length < 15">Twoj adres e-mail jest w sam raz.</div>
-	<div v-else>Twoj adres e-mail jest stanowczo za dlugi.</div>
+	<div v-if="authenticatedUsername">
+	<h3> zalogowany jako {{ authenticatedUsername }} </h3>
+	<a @click="logMeOut()">wyloguj</a>
 	
-		<input type="email"
-			v-model="email">
-			
-	<button @click="alertMyEmail()">Wyswietl moj e-mail w alercie</button>
+		
+	</div>
+
 	
+
+
+	<div v-else>
+			<input type="email" v-model="email">
+	<button @click="logMeIn()">Zaloguj sie</button>
+		
+	</div>
+
+		
   </div>
 </template>
 
@@ -24,13 +32,20 @@
 export default {
 	data() {
  		return {
- 			email: ''
+ 			email: '',
+ 			authenticatedUsername: ''
  		};
+
+ 		
  	},
  	methods: {
-  		alertMyEmail() {
-    		alert(this.email);
-  		}
+  		logMeIn() {
+    		this.authenticatedUsername = this.email;
+    		this.email = '';
+  		},
+  		 	logMeOut() {
+ 		this.autenticatedUsername = '';
+ 		}
 	}
 }
 
