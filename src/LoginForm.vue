fc55b93dd1b0443d071c76ee3c3ef9559e80fbca
mhendzel123 <mhendzel@wp.pl>
2020-04-25 12:04:39 +0200

    computed

--- src/LoginForm.vue
+++ src/LoginForm.vue
@@ -1,23 +1,34 @@
 <template>
-    <div>
-        <label>Zaloguj się e-mailem</label>
-        <input type="email" v-model="email">
-        <button @click="enter()">{{ buttonLabel }}</button>
-    </div>
+  <div>
+    <label>Zaloguj się e-mailem</label>
+    <input type="email" v-model="email" />
+    <button @click="enter()">{{ buttonLabel }}</button>
+  </div>
 </template>
 
 <script>
-    export default {
-        props: ['buttonLabel'],
-        data() {
-            return {
-                email: ''
-            }
-        },
-        methods: {
-            enter() {
-                this.$emit('login', this.email);
-            }
-        }
+export default {
+  props: ["buttonLabel"],
+  data() {
+    return {
+      email: ""
+    };
+  },
+  methods: {
+    enter() {
+      this.$emit("login", this.email);
     }
+  },
+  //     mounted() {
+  // 	if (!this.buttonLabel) {
+  // 		this.buttonLabel = 'Zaloguj sie';
+  // 	}
+  // },
+
+  computed: {
+    buttonLabelToDisplay() {
+      return this.buttonLabel || "Zaloguj si�";
+    }
+  }
+}
 </script>
