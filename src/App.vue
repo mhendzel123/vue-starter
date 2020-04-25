a41f3cff9ed2ce92371f599f5158b8fde8a7dfe6
mhendzel123 <mhendzel@wp.pl>
2020-04-25 12:07:16 +0200

    computed3

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
     <div>
         <h1>Witaj w systemie do zapisów na zajęcia</h1>
 
@@ -13,27 +12,9 @@
         </div>
 
     </div>
-=======
-<div>
-
-	<h1>Witaj w systemie zapisow</h1>	
-		
-	<div v-if="authenticatedUsername">
-		<h3>Zalogowany jako: {{ authenticatedUsername }}</h3>
-		<a @click="logMeOut()">Wyloguj</a>
-	</div>
-
-	<div v-else>
-		<login-form @login="logMeIn($event)"></login-form>		
-	</div>
-	
-</div>
->>>>>>> 815caf2d8e7d9895b2f021217e38d6fbc2621f07
 </template>
 
-
 <script>
-<<<<<<< HEAD
     import "milligram";
     import LoginForm from "./LoginForm";
 
@@ -54,31 +35,3 @@
         }
     }
 </script>
-=======
-import "milligram";
-import LoginForm from "./LoginForm";
-
-export default {
-	components: {LoginForm},
-	data() {
-		return {
-			email: '',
-			authenticatedUsername: ''
-		};
-	},
-	methods: {
-		logMeIn(username) {
-			this.authenticatedUsername = username;
-		},
-		logMeOut() {
-			this.authenticatedUsername = '';
-		}
-	}
-}
-</script>
-
-
-
-<style>
-</style>
->>>>>>> 815caf2d8e7d9895b2f021217e38d6fbc2621f07
