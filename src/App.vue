76bdc3c0d6c1d39f55a5f8a2e1eed2a198323eed
mhendzel123 <mhendzel@wp.pl>
2020-04-25 11:12:33 +0200

    mill

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,40 @@
-
-
 <template>
-  <div>
-  
-	<h1>Witaj w systemie zapisow</h1>
-	
-	<div v-if="authenticatedUsername">
-	<h3> zalogowany jako {{ authenticatedUsername }} </h3>
-	<a @click="logMeOut()">wyloguj</a>
-	
+<div>
+	<h1>Witaj w systemie zapisów</h1>	
 		
+	<div v-if="authenticatedUsername">
+	<h3>Zalogowany jako: {{ authenticatedUsername }}</h3>
+	<a @click="logMeOut()">Wyloguj</a>
 	</div>
-
-	
-
 
 	<div v-else>
-			<input type="email" v-model="email">
-	<button @click="logMeIn()">Zaloguj sie</button>
-		
+		<input type="email" v-model="email">
+		<button @click="logMeIn()">Zaloguj siê</button>
 	</div>
-
-		
-  </div>
+</div>
 </template>
 
-
-
 <script>
+import "milligram";
 
-export default {
-	data() {
- 		return {
- 			email: '',
- 			authenticatedUsername: ''
- 		};
-
- 		
- 	},
- 	methods: {
-  		logMeIn() {
-    		this.authenticatedUsername = this.email;
-    		this.email = '';
-  		},
-  		 	logMeOut() {
- 		this.autenticatedUsername = '';
- 		}
+	export default {
+		data() {
+		return {
+			email: '',
+			authenticatedUsername: ''
+		};
+	},
+		methods: {
+		logMeIn() {
+			this.authenticatedUsername = this.email;
+			this.email = '';
+		},
+		logMeOut() {
+			this.authenticatedUsername = '';
+		}
 	}
 }
-
 </script>
 
-
-
 <style>
-
 </style>
